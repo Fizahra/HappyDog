f7a9e7c59d6bbd5e9e0a4fd1ebfcce1f4b28a1bd
FITRI AZZAHRA <fitriazzahra3@gmail.com>
2023-09-29 18:01:25 +0700

    add logout user

--- app/src/main/java/com/example/happydog/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/happydog/ui/fragment/profile/ProfileFragment.kt
@@ -1,5 +1,8 @@
 package com.example.happydog.ui.fragment.profile
 
+import android.app.AlertDialog
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +10,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.happydog.R
 import com.example.happydog.databinding.FragmentProfileBinding
+import com.example.happydog.ui.auth.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
+    private lateinit var fbAuth : FirebaseAuth
+    lateinit var pd : ProgressDialog
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +36,28 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        fbAuth = FirebaseAuth.getInstance()
+        pd = ProgressDialog(activity)
+
+        binding.button.setOnClickListener{
+            logOut()
+        }
+
         return root
+    }
+
+    private fun logOut(){
+        val ad = getActivity()?.let { AlertDialog.Builder(it) }
+        ad?.setTitle(getString(R.string.logout_confirm))
+            ?.setPositiveButton(getString(R.string.yes)){ _, _ ->
+                fbAuth.signOut()
+                val intent = Intent(getActivity(), LoginActivity::class.java)
+                getActivity()?.startActivity(intent)
+                activity?.finish()
+            }
+            ?.setNegativeButton(getString(R.string.no), null)
+        val alert = ad?.create()
+        alert?.show()
     }
 
     override fun onDestroyView() {
