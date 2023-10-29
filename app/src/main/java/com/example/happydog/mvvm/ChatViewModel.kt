8dc2c5f10e0d66ea44d9cb8a857ce7e262a0f545
FITRI AZZAHRA <fitriazzahra3@gmail.com>
2023-10-29 17:16:35 +0700

    send message blm jdi

--- app/src/main/java/com/example/happydog/mvvm/ChatViewModel.kt
+++ app/src/main/java/com/example/happydog/mvvm/ChatViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.happydog.mvvm
 
 import androidx.lifecycle.LiveData
-import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -49,4 +48,163 @@ class ChatViewModel : ViewModel() {
 //            }
 //    }
 
+    fun sendMessage(sender: String, receiver: String, friendname: String, friendimage: String) =
+        viewModelScope.launch(Dispatchers.IO) {
+
+            val context = MyApp.instance.applicationContext
+
+            val hashMap = hashMapOf<String, Any>(
+                "sender" to sender,
+                "receiver" to receiver,
+                "message" to message.value!!,
+                "time" to Utils.getTime()
+            )
+
+
+            val uniqueId = listOf(sender, receiver).sorted()
+            uniqueId.joinToString(separator = "")
+
+
+            val friendnamesplit = friendname.split("\\s".toRegex())[0]
+            val mysharedPrefs = SharedPrefs(context)
+            mysharedPrefs.setValue("friendid", receiver)
+            mysharedPrefs.setValue("chatroomid", uniqueId.toString())
+            mysharedPrefs.setValue("friendname", friendnamesplit)
+            mysharedPrefs.setValue("friendimage", friendimage)
+
+
+
+
+            firestore.collection("Messages").document(uniqueId.toString()).collection("chats")
+                .document(Utils.getTime()).set(hashMap).addOnCompleteListener {
+
+
+                    val setHashap = hashMapOf<String, Any>(
+                        "friendid" to receiver,
+                        "time" to Utils.getTime(),
+                        "sender" to Utils.getUidLoggedIn(),
+                        "message" to message.value!!,
+                        "friendsimage" to friendimage,
+                        "name" to friendname,
+                        "person" to "you"
+                    )
+
+
+                    firestore.collection("Conversation${Utils.getUidLoggedIn()}").document(receiver)
+                        .set(setHashap)
+
+
+
+                    firestore.collection("Conversation${receiver}").document(Utils.getUidLoggedIn())
+                        .update(
+                            "message",
+                            message.value!!,
+                            "time",
+                            Utils.getTime(),
+                            "person",
+                            name.value!!
+                        )
+
+
+
+//                    firestore.collection("Tokens").document(receiver).addSnapshotListener { value, error ->
+//
+//
+//                        if (value != null && value.exists()) {
+//
+//
+//                            val tokenObject = value.toObject(Token::class.java)
+//
+//
+//                            token = tokenObject?.token!!
+//
+//
+//                            val loggedInUsername =
+//                                mysharedPrefs.getValue("username")!!.split("\\s".toRegex())[0]
+//
+//
+//
+//                            if (message.value!!.isNotEmpty() && receiver.isNotEmpty()) {
+//
+//                                PushNotification(
+//                                    NotificationData(loggedInUsername, message.value!!), token!!
+//                                ).also {
+//                                    sendNotification(it)
+//                                }
+//
+//                            } else {
+//
+//
+//                                Log.e("ChatAppViewModel", "NO TOKEN, NO NOTIFICATION")
+//                            }
+//
+//
+//                        }
+//
+//                        Log.e("ViewModel", token.toString())
+//
+//
+//
+//                        if (taskmessage.isSuccessful){
+//
+//                            message.value = ""
+//
+//
+//
+//                        }
+//
+//
+//                    }
+                }
+
+
+
+
+
+        }
+
+//    fun sendMessage(sender: String, receiver: String, friendname: String, friendimage: String) =
+//        viewModelScope.launch(Dispatchers.IO) {
+//            val ctx = MyApp.instance.applicationContext
+//            val hashMap = hashMapOf<String, Any>(
+//                "sender" to sender, "receiver" to receiver, "message" to message.value!!, "time" to Utils.getTime()
+//            )
+//            val uniqueId = listOf(sender,receiver).sorted()
+//            uniqueId.joinToString(separator = "")
+//
+//            val friendnamesplit = friendname.split("\\s".toRegex())[0]
+//            val sharedPref = SharedPrefs(ctx)
+//            sharedPref.setValue("friendid", receiver)
+//            sharedPref.setValue("chatroomid", uniqueId.toString())
+//            sharedPref.setValue("friendname", friendnamesplit)
+//            sharedPref.setValue("friendimage", friendimage)
+//
+//            firestore.collection("Messages").document(uniqueId.toString()).collection("chats").document(Utils.getTime()).set(hashMap).addOnCompleteListener{
+//                task ->
+//                val setHashap = hashMapOf<String, Any>(
+//                    "friendid" to receiver,
+//                    "time" to Utils.getTime(),
+//                    "sender" to Utils.getUidLoggedIn(),
+//                    "message" to message.value!!,
+//                    "friendsimage" to friendimage,
+//                    "name" to friendname,
+//                    "person" to "you"
+//                )
+//
+//                firestore.collection("Conversation${Utils.getUidLoggedIn()}").document(receiver)
+//                    .set(setHashap)
+//
+//                firestore.collection("Conversation${receiver}").document(Utils.getUidLoggedIn())
+//                    .update(
+//                        "message",
+//                        message.value!!,
+//                        "time",
+//                        Utils.getTime(),
+//                        "person",
+//                        name.value!!
+//                    )
+//
+//
+//            }
+//        }
 }
--- app/src/main/java/com/example/happydog/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/example/happydog/ui/chat/ChatActivity.kt
@@ -1,17 +1,23 @@
 package com.example.happydog.ui.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.happydog.R
+import com.example.happydog.Utils
 import com.example.happydog.databinding.ActivityChatBinding
 import com.example.happydog.model.Users
+import com.example.happydog.mvvm.ChatViewModel
+import com.example.happydog.ui.fragment.home.HomeFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatBinding
+    lateinit var vm : ChatViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
@@ -21,10 +27,18 @@ class ChatActivity : AppCompatActivity() {
         val nama = users.username
 //        users = intent.getParcelableExtra("UserId")
         Log.d("users","ini username $nama")
+        vm = ViewModelProvider(this).get(ChatViewModel::class.java)
 
         binding.tvName.text = nama
         binding.tvStatus.text = users.status
         Glide.with(this).load(users.imageUrl).placeholder(R.drawable.logo_happyvet).into(binding.imgUser)
+        binding.imgBack.setOnClickListener{
+            val intent = Intent(this, HomeFragment::class.java)
+            startActivity(intent)
+        }
+        binding.btnSend.setOnClickListener{
+            vm.sendMessage(Utils.getUidLoggedIn(), users.userid!!, users.username!!, users.imageUrl!! )
+        }
         val firebase = FirebaseAuth.getInstance().currentUser
 //        val reference = FirebaseFirestore.getInstance().document()
 //            FirebaseDatabase.getInstance().getReference("users").child(users)
